629ef5a441baf6622a47c549ac6fd45ad4edc983
vedatozayar <vedatozayar@gmail.com>
2022-09-16 03:57:04 +0300

    ready to deploy

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,73 @@
+import Head from 'next/head';
+import { MongoClient, ObjectId } from 'mongodb';
+import { Fragment } from 'react';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = (props) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name='description' content={props.meetupData.description}></meta>
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
+  );
+};
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://Vedat:5AR7rvvqeNGwm7fn@cluster0.u1az5hr.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: 'blocking',
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    'mongodb+srv://Vedat:5AR7rvvqeNGwm7fn@cluster0.u1az5hr.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+//api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://Vedat:5AR7rvvqeNGwm7fn@cluster0.u1az5hr.mongodb.net/?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted' });
+  }
+}
+
+export default handler;
